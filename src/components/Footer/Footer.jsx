d0a2046e63d5570a87936fe4066647a4629f533d
Jose Germán Martínez <jgermanmm@gmail.com>
2023-03-20 12:36:48 -0300

    Completar info del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,10 @@ import { FooterStyle } from './styles.js'
 export default function Footer() {
   return (
     <FooterStyle>
-        Proyecto desarrollado por José G. Martínez en Escalab - Módulo ReactJs
+        <h3>Proyecto desarrollado por José G. Martínez en Escalab - Módulo ReactJs</h3>
+        <p>Carateríticas:</p>
+        <p>Consumos de APIs, manipulación de datos mediante hooks de react, comunicación entre componentes (padre-hijo), styled components y estruturas de códigos limpias</p>
+        <a href='https://github.com/JoseGermanx/react-comunicacion-api'>Link a repositorio</a>
     </FooterStyle>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const  FooterStyle = styled.div`
+font-size: 12px;
+padding: 10px;
 color: white;
 background-color: #154c79;
 font-style: italic;
