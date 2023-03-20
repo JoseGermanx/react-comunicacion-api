60dcacfdf7726034da84ba24fc114c16676bbaab
Jose Germán Martínez <jgermanmm@gmail.com>
2023-03-20 20:29:07 -0300

    Personalizacion de config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,27 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    port: 4000,
+  },
+  preview: {
+    port: 4270,
+  },
+  build: {
+    incremental: true,
+    babel: {
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    },
+    
+    cache: true,
+    minify: true,
+    mode: "production",
+    chunks: true,
+    moduleBundling: true,
+    prerenderPaths: ["/"],
+    modulePreload: true,
+    outDir: "build",
+    devCode: true,
+    debug: true,
+  },})
