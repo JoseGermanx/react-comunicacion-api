7a044dd5e19a690bf928a29e743e988ebc63fc85
Jose Germán Martínez <jgermanmm@gmail.com>
2023-03-20 12:40:38 -0300

    Correccion ortográfica

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <FooterStyle>
         <h3>Proyecto desarrollado por José G. Martínez en Escalab - Módulo ReactJs</h3>
-        <p>Carateríticas:</p>
+        <p>Caracteríticas:</p>
         <p>Consumos de APIs, manipulación de datos mediante hooks de react, comunicación entre componentes (padre-hijo), styled components y estruturas de códigos limpias</p>
         <a href='https://github.com/JoseGermanx/react-comunicacion-api'>Link a repositorio</a>
     </FooterStyle>
